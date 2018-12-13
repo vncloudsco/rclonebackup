01052c57edd717b7bb3ce371afaf833542a373ec
vncloudsco <support@vnclouds.edu.vn>
2018-12-13 14:36:58 +0700

    Update rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -7,30 +7,30 @@ EE="$(ls /etc/ | grep ee)"
 Cyber="$(ls /etc/ | grep cyberpanel)"
 
 if [[ "$SENTORA" = "sentora" ]]; then
-	wget -P /root/.config/script/ http://rclone.vnclouds.co/sentora/rclone.sh
+	wget -P /root/.config/script/ https://rclone.vnclouds.co/sentora/rclone.sh
 
 elif [[ "$KUSANAGI" "kusanagi" ]]; then
-	wget -P /root/.config/script/ http://rclone.vnclouds.co/kusanagi/rclone.sh
+	wget -P /root/.config/script/ https://rclone.vnclouds.co/kusanagi/rclone.sh
 
 elif [[ "$CWP" "cwpsrv" ]]; then
-	wget -P /root/.config/script/ http://rclone.vnclouds.co/CWP/rclone.sh
+	wget -P /root/.config/script/ https://rclone.vnclouds.co/CWP/rclone.sh
 
 
 elif [[ "$DA" = "directadmin" ]]; then
-	wget -P /root/.config/script/ http://rclone.vnclouds.co/directadmin/rclone.sh
+	wget -P /root/.config/script/ https://rclone.vnclouds.co/directadmin/rclone.sh
 
 
 elif [[ "$VESTA" = "vesta" ]]; then
-	wget http://rclone.vnclouds.co/vesta/rclone.sh
+	wget -P /root/.config/script/ https://rclone.vnclouds.co/vesta/rclone.sh
 	
 elif [[ "$EE" = "ee" ]]; then
-	wget -P /root/.config/script/ http://rclone.vnclouds.co/ee/rclone.sh
+	wget -P /root/.config/script/ https://rclone.vnclouds.co/ee/rclone.sh
 
 elif [[ "$Cyber" = "cyberpanel" ]]; then
-	wget -P /root/.config/script/ http://rclone.vnclouds.co/cyberpanel/rclone.sh
+	wget -P /root/.config/script/ https://rclone.vnclouds.co/cyberpanel/rclone.sh
 
 else
-	wget -P /root/.config/script/ http://rclone.vnclouds.co/vnc/rclone.sh
+	wget -P /root/.config/script/ https://rclone.vnclouds.co/vnc/rclone.sh
 
 
 fi
