be52bb7b4167b7f836d56bc121053c064a020f6d
vncloudsco <support@vnclouds.edu.vn>
2018-12-14 16:38:49 +0700

    Update rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -10,7 +10,8 @@ if [[ "$SENTORA" = "sentora" ]]; then
 	wget -P /root/.config/script/ https://rclone.vnclouds.co/sentora/rclone.sh
 
 elif [[ "$KUSANAGI" "kusanagi" ]]; then
-	wget -P -O /root/.config/script/rclone.sh https://rclone.vnclouds.co/kusanagi/kusanagi.sh
+	wget -P /root/.config/script/rclone.sh https://rclone.vnclouds.co/kusanagi/kusanagi.sh
+	mv /root/.config/script/kusanagi.sh /root/.config/script/rclone.sh
 
 elif [[ "$CWP" "cwpsrv" ]]; then
 	wget -P /root/.config/script/ https://rclone.vnclouds.co/CWP/rclone.sh
@@ -34,6 +35,6 @@ else
 
 
 fi
-
+chmod 777 /root/.config/script/rclone.sh
 crontab -l | { cat; echo "0 1 * * *  /root/.config/script/rclone.sh"; } | crontab -
 rm -f rclone.sh
